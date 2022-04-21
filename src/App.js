43fa67d99d506055781cc3f486ea6165c2f8b928
Taniya Verma <70065103+taniya1027@users.noreply.github.com>
2022-04-21 21:03:46 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ const App = () => {
     return (
         <ChatEngine 
            height = "100vh"
-           projectID = "7a704088-98dc-4203-a0f7-cc2159f81d85"
+           projectID = "f952b7fe-7e01-4455-b308-2541bc7b3257"
            userName = {localStorage.getItem('username')}
            userSecret = {localStorage.getItem('password')}
            renderChatFeed = {(ChatAppProps) => <ChatFeed {... ChatAppProps} />}
